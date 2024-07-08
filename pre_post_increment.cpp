40e54e70a051187caa4687b0e6b774e2756e3c84
Roshan275 <roshanjamsandekar10@gmail.com>
2024-07-09 02:27:13 +0530

    Create pre_post_increment.cpp

---
+++ pre_post_increment.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int i=20;
+    cout<<"Initial value: "<<20<<endl;
+    cout<<"During execution of post increment: "<<i++<<endl;
+    cout<<"After post increment: "<<i<<endl<<endl;
+    
+    int j=20;
+    cout<<"Initial value: "<<20<<endl;
+    cout<<"During Execution of pre increment: "<<++j<<endl;
+    cout<<"After pre increment: "<<j<<endl;
+}
