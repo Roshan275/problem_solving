45dfcf1076a0403135b38df88708f24bbaee07e6
Roshan275 <roshanjamsandekar10@gmail.com>
2024-07-12 22:09:17 +0530

    oddEven.cpp

--- oddEven.cpp
+++ oddEven.cpp
@@ -2,20 +2,24 @@
 using namespace std;
 
 int main() {
-    int num;
+    int num1,num2;
 
-    // Input a number
-    cout << "Enter an integer: ";
-    cin >> num;
+    cout << "Enter first number: ";
+    cin >> num1;
+    cout << "Enter second number: ";
+    cin >> num2;
 
     // Check if the number is even or odd
-    if (num % 2 == 0) 
+    if (num1 % 2 == 0 && num2%2==0) 
     {
-        cout << num << " is even." << endl;
+        cout <<"Both are Even"<< endl;
     } 
-    else 
+    else if (num1%2==0){
+        cout << num1 << " is Even and "<< num2<<" is odd"<<endl;
+    }
+    else
     {
-        cout << num << " is odd." << endl;
+        cout << "both are odd"<< endl;
     }
 
     return 0;
